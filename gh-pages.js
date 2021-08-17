790940d6aaf22d35b97c86f493562a3009a2c89f
Sebastian <sebastian.zawolanski@gmail.com>
2021-08-17 18:43:07 +0200

    Initial gh-pages commit

---
+++ gh-pages.js
@@ -0,0 +1,16 @@
+const ghpages = require('gh-pages');
+
+ghpages.publish(
+  'public',
+  {
+    branch: 'production',
+    repo: 'https://github.com/zawolanski/tip-calculator.git',
+    user: {
+      name: 'zawolanski',
+      email: 'sebastian.zawolanski@gmail.com',
+    },
+  },
+  () => {
+    console.log('Deploy Complete!');
+  },
+);
